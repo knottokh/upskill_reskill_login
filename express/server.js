91379cd5978a079749fab18b1046c0a838b8d131
knottokh <doctortony35@hotmail.com>
2021-06-27 00:48:52 +0700

    Update server.js

--- express/server.js
+++ express/server.js
@@ -72,14 +72,43 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+  //console.log('ok');
+  res.sendFile(path.join(__dirname, '../dist/index3.html'));
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
+router.post('/login', function(req, res) {
+  if (!(req.body.username === 'john.doe' && req.body.password === 'foobar')) {
+    res.status(401).send('Wrong user or password');
+    console.log('failed login');
+    return;
+  }
+  console.log('successful login');
+  // We are sending the profile inside the token
+  var token = jwt.sign({ firstname: 'John', lastname: 'Doe'}, secret, { expiresIn: 5 * 60 });
+  res.json({ token: token });
+});
+
+
+router.get('/api/profile', function (req, res) {
+  console.log('user ' + req.user.firstname + ' is calling /api/profile');
+  res.json({
+    name: req.user.firstname
+  });
+});
+
+app.use('/api', expressJwt({secret: secret}));
+
+app.use(function(err, req, res, next){
+  if (err.constructor.name === 'UnauthorizedError') {
+    res.status(401).send('Unauthorized');
+  }
+});
+
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../dist/index2.html')));
 
