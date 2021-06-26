4d23dc82f88da444cbb5e074431cc62a1bf57d49
knottokh <doctortony35@hotmail.com>
2021-06-26 23:58:51 +0700

    Update server.js

--- express/server.js
+++ express/server.js
@@ -16,20 +16,11 @@ app.use(bodyParser.json());
 // app.use('/', express.static(__dirname + '/'));
 app.use('/', express.static(path.join(__dirname, 'dist')));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
-});
-
 app.post('/login', function(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+
   if (!(req.body.username === 'john.doe' && req.body.password === 'foobar')) {
     res.status(401).send('Wrong user or password');
     console.log('failed login');
@@ -51,29 +42,12 @@ app.use(function(err, req, res, next){
 });
 
 app.get('/api/profile', function (req, res) {
-  console.log('user ' + req.user.firstname + ' is calling /api/profile');
-  res.json({
-    name: req.user.firstname
-  });
-});
-
-app.use((req, res, next) => {
-  //const error = new Error("Not found");
-  //error.status = 404;
-  //next(error);
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
-
-app.use((error, req, res, next) => {
-  //res.sendFile(path.join(__dirname, 'dist/index.html'));
-  res.status(error.status || 500);
+  console.log('user ' + req.user.firstname + ' is calling /api/profile');
   res.json({
-    error: {
-      message: error.message
-    }
+    name: req.user.firstname
   });
 });
 
