4d5ee03bf4620dfd2b1f2b4eb345c457dcfdb0d6
knottokh <doctortony35@hotmail.com>
2021-06-27 11:19:26 +0700

    Update server.js

--- express/server.js
+++ express/server.js
@@ -83,7 +83,8 @@ router.get('/another', (req, res) => {
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+//app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', router);  
 
 module.exports = app;
 module.exports.handler = serverless(app);
